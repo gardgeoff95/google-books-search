059f106a05aa95d2dbacbdcef7961fed34e8d31d
Geoff <gardgeoff95@email.arizona.edu>
2019-09-23 23:05:43 -0700

    added more routes still can't get db

---
+++ client/build/precache-manifest.14d735c58b04d014ec52444c4a85e850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f5808260db65fbe2e6aeb69693339c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5026ef495108afe32b0",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "2c19d08396717eb12c22",
+    "url": "/static/js/2.b934aa8c.chunk.js"
+  },
+  {
+    "revision": "f5026ef495108afe32b0",
+    "url": "/static/js/main.b3b684f5.chunk.js"
+  },
+  {
+    "revision": "303353ecbb23dac4b8e8",
+    "url": "/static/js/runtime-main.ad259024.js"
+  }
+]);
